b6be205c516f0834ebb61a2157de5774225b9256
Tutul Kabir <tutulkabir@gmail.com>
2024-06-15 14:33:07 +0530

    built a search bar that filters a list of items based on user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Eight from "./components/Eight";
 import Eleven from "./components/Eleven";
+import Fifteen from "./components/Fifteen";
 import Five from "./components/Five";
 import Four from "./components/Four";
 import Fourteen from "./components/Fourteen";
@@ -14,9 +15,16 @@ import Twelve from "./components/Twelve";
 import Two from "./components/Two";
 
 function App() {
+  const items = [
+    "Item 1",
+    "Item 2",
+    "Item 3",
+    "Another Item",
+    "Another item 2",
+  ];
   return (
     <>
-      <Fourteen />
+      <Fifteen items={items} />
     </>
   );
 }
